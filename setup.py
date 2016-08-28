09e63c5f828afb94af4969d73bd40cf1a72c857c
Ramon Wijnands <rayman747@hotmail.com>
2016-08-28 23:20:12 +0200

    Create a tue_tools package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='tue_tools',
+    packages=find_packages('src'),  # include all packages under src
+    package_dir={'': 'src'},  # tell distutils packages are under src
+    install_requires=['rosdistro'],
+    scripts=['scripts/tue-get']
+)
