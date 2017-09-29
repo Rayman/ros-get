99d7025a2cd268ff17f0a1c0330520dc93f8a4b4
Ramon Wijnands <rayman747@hotmail.com>
2017-09-29 16:35:27 +0200

    Copy the command parsing from the master

--- src/ros_get/__main__.py
+++ src/ros_get/__main__.py
@@ -1,5 +1,76 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import logging.config
+from argparse import ArgumentParser
+
+from ros_get.commands import install, update, upgrade, remove
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'colored': {
+            '()': 'colorlog.ColoredFormatter',
+            'format':
+                "%(log_color)s[%(levelname)s] %(name)s: %(message)s",
+        }
+    },
+    'handlers': {
+        'stream': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'colored',
+        },
+    },
+    'loggers': {
+        'vcstool.executor': {
+            'level': 'INFO',
+        }
+
+    },
+    'root': {
+        'handlers': ['stream'],
+        'level': 'INFO',
+    },
+})
+
+
+def not_implemented():
+    raise NotImplementedError()
+
+
 def main():
-    print 'hoi'
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true')
+
+    subparsers = parser.add_subparsers()
+
+    subparser = subparsers.add_parser('install', help='install packages')
+    subparser.set_defaults(func=install)
+    subparser.add_argument('pkgs', nargs='+', metavar='pkg')
+
+    subparser = subparsers.add_parser('update', help='update list of available packages')
+    subparser.set_defaults(func=update)
+
+    subparser = subparsers.add_parser('upgrade', help='upgrade the workspace by installing/upgrading packages')
+    subparser.set_defaults(func=upgrade)
+
+    subparser = subparsers.add_parser('remove', help='remove packages')
+    subparser.set_defaults(func=remove)
+    subparser.add_argument('pkgs', nargs='+', metavar='pkg')
+
+    subparser = subparsers.add_parser('autoremove', help='remove automatically all unused packages')
+    subparser.set_defaults(func=not_implemented)
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    func = args.func
+    del args.func
+    exit(func(**vars(args)))
+
 
 if __name__ == '__main__':
     main()
---
+++ src/ros_get/commands.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+
+def install(pkgs, verbose):
+    pass
+
+
+def update(verbose):
+    pass
+
+
+def remove(pkgs, verbose):
+    pass
+
+
+def upgrade(verbose):
+    pass
