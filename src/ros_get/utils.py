6bb0f38bf6f8924092255230894f2ba297d2e76b
Ramon Wijnands <rayman747@hotmail.com>
2017-10-01 16:07:31 +0200

    Copy get_rosdep

--- src/ros_get/utils.py
+++ src/ros_get/utils.py
@@ -1,6 +1,9 @@
 import errno
 import logging
 import os
+from rosdep2 import RosdepLookup
+from rosdep2 import create_default_installer_context, get_default_installer
+from rosdep2.rospkg_loader import DEFAULT_VIEW_KEY
 
 logger = logging.getLogger(__name__)
 
@@ -26,3 +29,30 @@ def symlink_force(target, link_name):
             os.symlink(target, link_name)
         else:
             raise e
+
+
+cached_view = None
+
+
+def get_rosdep(key):
+    installer_context = create_default_installer_context(verbose=False)
+
+    installer, installer_keys, default_key, os_name, os_version = get_default_installer(
+        installer_context=installer_context, verbose=False)
+
+    global cached_view
+    if not cached_view:
+        lookup = RosdepLookup.create_from_rospkg()
+        lookup.verbose = False
+
+        view = lookup.get_rosdep_view(DEFAULT_VIEW_KEY, verbose=False)
+        cached_view = view
+    else:
+        view = cached_view
+
+    try:
+        d = view.lookup(key)
+        rule_installer, rule = d.get_rule_for_platform(os_name, os_version, installer_keys, default_key)
+        return rule_installer, rule
+    except KeyError as e:
+        return False
