f45277004f19193d0c82d4548320a5f438a290b3
Ramon Wijnands <rayman747@hotmail.com>
2017-10-06 23:00:42 +0200

    Determine dirs

--- src/ros_get/commands.py
+++ src/ros_get/commands.py
@@ -2,12 +2,19 @@
 from __future__ import print_function
 
 import logging
+import os
+
+from .utils import mkdir_p
+from .workspace import ws_file
 
 logger = logging.getLogger(__name__)
+installed_dir = os.path.join(ws_file, '.env', 'installed')
+target_path = os.path.join(ws_file, 'src')
 
 
 def install(pkgs, verbose):
-    pass
+    pkgs_queue = list(pkgs)
+    print('TODO: %s' % pkgs)
 
 
 def update(verbose):
@@ -20,3 +27,12 @@ def remove(pkgs, verbose):
 
 def upgrade(verbose):
     pass
+
+
+def add_pkgs_to_installed_list(pkgs):
+    mkdir_p(installed_dir)
+
+    # touch the file
+    for pkg in pkgs:
+        logger.debug('marking for installation: %s', pkg)
+        open(os.path.join(installed_dir, pkg), 'a').close()
