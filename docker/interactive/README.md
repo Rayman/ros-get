1b3bc7a89f81e9df9578d3a1dfe570e65d698b9b
Ramon Wijnands <rayman747@hotmail.com>
2017-12-27 16:46:34 +0100

    Add an interactive demo with docker

---
+++ docker/interactive/README.md
@@ -0,0 +1,11 @@
+# Interactive ros-get demo
+
+```sh
+cd docker/interactive/
+docker build -t ros-get .
+docker run -it ros-get
+```
+Now you're in an interacive shell with a ros-get workspace setup for you. You can now test it out. For example:
+```sh
+ros-get update
+```
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # this file is in the root of a workspace
 _WS_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 
-export ROSDISTRO_INDEX_URL=file://$_WS_DIR/rosdistro/index.yaml
+export ROSDISTRO_INDEX_URL=$(<$_WS_DIR/.ros-get/rosdistro_index_url)
 export ROS_ETC_DIR=$_WS_DIR/.etc
 export ROS_HOME=$_WS_DIR/home
 
