a40ac682b17d2c9d0e8b1e7ce4557335a15b9247
Ramon Wijnands <rayman747@hotmail.com>
2018-04-13 15:56:09 +0200

    Add more info to setup.py for release to PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
+import os
+import re
 from setuptools import setup, find_packages
+
+
+def get_ini_variable(name):
+    with open(os.path.join(os.path.dirname(__file__), 'src', 'ros_get', '__init__.py')) as f:
+        return re.compile(r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as r_file:
+    readme = r_file.read()
 
 setup(
     name='ros_get',
+    version=get_ini_variable('__version__'),
+    url=get_ini_variable('__url__'),
+    author=get_ini_variable('__author__'),
+    author_email=get_ini_variable('__email__'),
+    description='Simple tools for working with ROS source packages',
+    long_description=readme,
     package_dir={'': 'src'},  # tell distutils packages are under src
     packages=find_packages('src'),  # include all packages under src
     install_requires=[
