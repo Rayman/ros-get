9ee219d85849629eac53a28e72fa374a6c805ea4
Ramon Wijnands <rayman747@hotmail.com>
2019-01-21 15:37:34 +0100

    Remove dependency that was fixed upstream

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'rosdep',
         'rosdistro >= 0.6.8',
         'rosinstall_generator',
-        'trollius',  # remove when catkin>0.4.4 is released
         'vcstools',
         'xdg==1.0.7',
     ],
