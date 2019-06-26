cb45210470ceb942f2772b5b677a2c16919708b4
Ramon Wijnands <rayman747@hotmail.com>
2019-06-26 16:13:44 +0200

    Bump version: 0.1.0 → 0.2.0-dev0

--- src/ros_get/__init__.py
+++ src/ros_get/__init__.py
@@ -1,7 +1,7 @@
 from .commands import install, update, status, list_packages, remove
 from .workspace import create, switch, save, list_workspaces, locate, name, rosdistro_url
 
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev0'
 __url__ = 'https://github.com/Rayman/ros-get'
 __author__ = 'Ramon Wijnands'
 __email__ = 'rayman747@hotmail.com'
