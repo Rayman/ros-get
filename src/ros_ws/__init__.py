54139600e2ef82d5823a66cc62910702a8041877
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2026-06-24 13:35:46 +0200

    Bump version: 0.6.0-dev2 → 0.6.0

--- src/ros_ws/__init__.py
+++ src/ros_ws/__init__.py
@@ -1,6 +1,6 @@
 from .workspace import create, switch, save, list_workspaces, locate, name
 
-__version__ = '0.6.0-dev2'
+__version__ = '0.6.0'
 __url__ = 'https://github.com/Rayman/ros-get'
 __author__ = 'Ramon Wijnands'
 __email__ = 'rayman747@hotmail.com'
